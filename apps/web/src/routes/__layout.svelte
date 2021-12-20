86490ac06dfe7e9877c9c25c2dbfcc4b5b9e7a6b
Tricked <72335827+SkyBlockDev@users.noreply.github.com>
2021-12-20 22:26:15 +0100

    add layout link

--- apps/web/src/routes/__layout.svelte
+++ apps/web/src/routes/__layout.svelte
@@ -43,6 +43,10 @@
 	];
 	const otherLinks = [
 		{
+			href: '/docs/effects',
+			a: 'Image effects'
+		},
+		{
 			href: '/docs/domain',
 			a: 'Adding your own domain'
 		},
@@ -131,47 +135,44 @@
 </svelte:head>
 
 <template bind:this={menu} class="flex-1 flex flex-col">
-  <nav>
-    <div class="all">
-      <a class="ascella" href="https://ascella.host">Ascella.host</a>
-      <div class="btns">
-        <ul>
-          <li><a class="btn" href="https://dash.ascella.host" > Dashboard </a></li>
-          <li><a class="btn" href="/"> Home </a></li>
-          <li><a class="btn" href="https://discord.gg/mY8zTARu4g" > Discord </a></li>
-        </ul>
-      </div>
-        <div class="other">
-          <input id="input" type="checkbox" checked>
-          <label for="input">Other
-            <svg class="w-5 h-5 -mr-1" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-            </svg>
-          </label>
-          <div class="list">
-            <ul>
-				    {#each otherLinks as link}
-              <li>
-                <div>
-                  <slide><a
-				  	  	    href={link.href}
-				  	    	  tabindex="0"
-				  	  	    role="menuitem">{link.a}</a
-				  	      ></slide>
-                  <a
-				  	  	    href={link.href}
-				  	    	  tabindex="0"
-				  	  	    role="menuitem">{link.a}</a
-				  	      >
-                </div>
-              </li>
-				    {/each}
-				    </ul>      
-          </div>
-        </div>
-      <a class="btn" href="/docs/signup">Create an account</a>
-    </div>
-  </nav>
+	<nav>
+		<div class="all">
+			<a class="ascella" href="https://ascella.host">Ascella.host</a>
+			<div class="btns">
+				<ul>
+					<li><a class="btn" href="https://dash.ascella.host"> Dashboard </a></li>
+					<li><a class="btn" href="/"> Home </a></li>
+					<li><a class="btn" href="https://discord.gg/mY8zTARu4g"> Discord </a></li>
+				</ul>
+			</div>
+			<div class="other">
+				<input id="input" type="checkbox" checked />
+				<label for="input"
+					>Other
+					<svg class="w-5 h-5 -mr-1" viewBox="0 0 20 20" fill="currentColor">
+						<path
+							fill-rule="evenodd"
+							d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</label>
+				<div class="list">
+					<ul>
+						{#each otherLinks as link}
+							<li>
+								<div>
+									<slide><a href={link.href} tabindex="0" role="menuitem">{link.a}</a></slide>
+									<a href={link.href} tabindex="0" role="menuitem">{link.a}</a>
+								</div>
+							</li>
+						{/each}
+					</ul>
+				</div>
+			</div>
+			<a class="btn" href="/docs/signup">Create an account</a>
+		</div>
+	</nav>
 
 	<div class="pb-4" />
 	<slot />
