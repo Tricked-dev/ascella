68eae0085d66e74b0874de3f32f76e4af069b819
Tricked <72335827+SkyBlockDev@users.noreply.github.com>
2022-01-03 18:06:49 +0100

    feat: ascella gui

--- apps/web/src/routes/docs/__layout.svelte
+++ apps/web/src/routes/docs/__layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import '../../css/github.scss';
+	import '@fontsource/righteous';
 	export const ssr = true;
 	export const prerender = true;
 	export const hydrate = false;
 	export const router = false;
 </script>
 
-<div class="max-w-[80rem] m-auto prose-invert prose-pink doc">
+<div
+	class="prose prose-emerald prose-invert doc max-w-[80rem] prose-headings:hover:text-emerald-500 m-auto lg:prose-2xl md:prose-lg doc prose-headings:no-underline"
+>
 	<slot />
 </div>
