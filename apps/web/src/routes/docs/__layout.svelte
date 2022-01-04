26936a9004ad035c58c501bc34cc609058d54c43
stef-the <stefthedoggo4@gmail.com>
2022-01-03 16:01:00 -0800

    Docs upgrade!!!

--- apps/web/src/routes/docs/__layout.svelte
+++ apps/web/src/routes/docs/__layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import '../../css/github.scss';
-	import '@fontsource/righteous';
 	export const ssr = true;
 	export const prerender = true;
 	export const hydrate = false;
@@ -8,7 +7,7 @@
 </script>
 
 <div
-	class="prose prose-emerald prose-invert doc max-w-[80rem] prose-headings:hover:text-emerald-500 m-auto lg:prose-2xl md:prose-lg doc prose-headings:no-underline"
+	class="max-w-[80rem] m-auto lg:prose-2xl md:prose-lg doc prose-headings:no-underline"
 >
 	<slot />
 </div>
