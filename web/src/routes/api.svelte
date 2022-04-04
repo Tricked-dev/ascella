603413c7a185382ce650ded869e79ca00f7f6cba
Tricked <72335827+SkyBlockDev@users.noreply.github.com>
2022-04-04 19:06:03 +0200

    swag

---
+++ web/src/routes/api.svelte
@@ -0,0 +1,19 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+
+	import SwaggerUISvelte from '../components/swagger/SwaggerUI.svelte';
+	export const load: Load = async ({ fetch }) => {
+		let swagger = await fetch('/swagger.json').then((r) => r.text());
+		return {
+			props: { swagger: JSON.parse(swagger) }
+		};
+	};
+</script>
+
+<script>
+	export let swagger;
+</script>
+
+<div class="bg-white">
+	<SwaggerUISvelte {swagger} url={'/swagger.json'} />
+</div>
