bae5b16026d26286ebe2251f8bd0b58b917856ef
Tricked <72335827+SkyBlockDev@users.noreply.github.com>
2022-04-19 07:43:20 +0200

    fix: rename to get

--- ascella/src/http/routes/v2/ascella_stats.rs
+++ ascella/src/http/routes/v2/ascella_stats.rs
@@ -5,7 +5,7 @@ use crate::prelude::*;
 ///
 /// provides some cool stats about ascella to use somewhere
 #[api_v2_operation(tags(Etc), produces = "application/json")]
-#[post("/stats.json")]
+#[get("/stats.json")]
 pub async fn get() -> Result<OkResponse<AscellaStats>, Error> {
   Ok(OkResponse(AscellaStats::new_with_stats().await))
 }
