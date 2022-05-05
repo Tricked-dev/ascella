f5a86d28abff52a9e185c5a57a0d02f74c861c7a
Tricked <72335827+SkyBlockDev@users.noreply.github.com>
2022-05-05 18:03:20 +0200

    fix: remove user caching

--- ascella/src/database/queries/get_user.rs
+++ ascella/src/database/queries/get_user.rs
@@ -1,6 +1,5 @@
 use crate::prelude::*;
 
-#[cached(size = 100, time = 120, result = true)]
 pub async fn exec(id: i32) -> Result<Users> {
   let row = get_tokio_postgres().await.query_one("SELECT * FROM users WHERE id = $1 LIMIT 1", &[&id]).await?;
 
